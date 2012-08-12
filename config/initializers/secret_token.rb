997c6eb487a8efffd1f9fae7d93f194b7fa9269c
tiseheaini <tiseheaini@gmail.com>
2012-08-12 12:04:15 +0800

    1 初始化完成 bundle .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coolxueyuan::Application.config.secret_token = '653bda3e71f59f4355fea519ef4d1b97302d5534de031ae27859c5f75bcd67abb45ededdee33f7190c780453ae412246fa861143d0330c9aae7356e1910cf654'
