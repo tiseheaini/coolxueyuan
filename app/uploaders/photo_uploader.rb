e69ee9d1a489a97df6c0a62dbd8848bbc3e55c02
tiseheaini <tiseheaini@gmail.com>
2012-08-31 22:39:19 +0800

    carrierwave 修改完成

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
-require 'carrierwave/processing/mini_magick'
+
 class PhotoUploader < CarrierWave::Uploader::Base
+
   # Include RMagick or MiniMagick support:
    include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -16,7 +17,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-	  'public/upload/'
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -26,6 +27,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+	  def default_url
+		  'rails.jpg'
+		end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -38,36 +42,23 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process :scale => [50, 50]
   # end
-	process :resize_to_limit => [64, 64]
-
-	version :thumb do
-	  process :resize_to_limit => [64, 64]
-	end
+	  version :thumb do
+		  process :resize_to_limit => [64, 64]
+		end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
   #   %w(jpg jpeg gif png)
   # end
-    def extension_white_list
-      %w(jpg jpeg gif png)
-    end
-		def default_url
-		  "rails.jpg"
+	  def extension_white_list
+		  %w(jpg jpeg gif png)
 		end
-					
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
-	def filename
-	  if super.present?          
-	    @name ||= Digest::MD5.hexdigest(File.dirname(current_path))
-	    "#{@name}.#{file.extension.downcase}"
-	  end
-	end
-				 
 
 end
