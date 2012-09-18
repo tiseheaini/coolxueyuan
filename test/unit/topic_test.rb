b13078f73466578a23acbbaae1356fdbbb9e0317
tiseheaini <tiseheaini@gmail.com>
2012-09-18 18:24:17 +0800

    给 topics 表写了一个小点的测试

--- test/unit/topic_test.rb
+++ test/unit/topic_test.rb
@@ -1,7 +1,11 @@
+# encoding: utf-8
 require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
+   test "topics 表测试验证" do
+		 t = Topic.new
+     assert !t.valid?
+     assert t.errors[:title]
+     assert t.errors[:text]
    end
 end
