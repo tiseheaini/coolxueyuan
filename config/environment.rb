206f4ab9822bf5499eba07132854903e18438a0f
tiseheaini <tiseheaini@gmail.com>
2012-10-02 22:28:49 +0800

    取消了 development 环境下的缓存

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Coolxueyuan::Application.initialize!
-ActionController::Base.cache_store = :file_store, "./public/view_cache"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ Coolxueyuan::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+	config.action_controller.page_cache_directory = "./public/page_cache"
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -41,6 +42,7 @@ Coolxueyuan::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
+  config.cache_store = :file_store, "./public/view_cache"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
